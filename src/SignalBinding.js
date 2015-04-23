cc589529fd17841c9b5ade584fb9f40d78f13cea
Mathieu Hofman <mathieu.hofman@citrix.com>
2015-04-23 12:21:29 -0700

    Guard against exceptions in listeners

--- src/SignalBinding.js
+++ src/SignalBinding.js
@@ -78,9 +78,16 @@
             var handlerReturn, params;
             if (this.active && !!this._listener) {
                 params = this.params? this.params.concat(paramsArr) : paramsArr;
-                handlerReturn = this._listener.apply(this.context, params);
-                if (this._isOnce) {
-                    this.detach();
+                try {
+                    handlerReturn = this._listener.apply(this.context, params);
+                } catch(e) {
+                    setTimeout(function() {
+                        throw e;
+                    }, 0);
+                } finally {
+                    if (this._isOnce) {
+                        this.detach();
+                    }
                 }
             }
             return handlerReturn;
